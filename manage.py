b06726d504967f3dd3bf9585c63304c49e02e013
NatashaSenah <tashsenah@gmail.com>
2018-09-14 12:15:18 +0300

    add the header in app/templates/index.html

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
-from app import create_app,db
-from flask_script import Manager,Server
-from app.models import User,Role,Comment
-app = create_app('development')
-
+# from app import create_app,db
+# from flask_script import Manager,Server
+# from app.models import User,Role,Comment
+# app = create_app('development')
+#
 manager = Manager(app)
-manager.add_command('server',Server)
+# manager.add_command('server',Server)
 
 
 
