eee08c63ed4202aa670c75b469e1d182eb72cd78
NatashaSenah <tashsenah@gmail.com>
2018-09-16 16:26:48 +0300

    add the modified app/main/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,21 @@
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
-from app import views
-from app import error
-# Initializing application
-app = Flask(__name__)
+from config import config_options
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
+bootstrap = Bootstrap()
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
+
 from app import views
